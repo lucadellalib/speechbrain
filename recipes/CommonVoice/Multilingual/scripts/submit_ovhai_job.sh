fea1055b09a58e4711907c8ff6f238c10351daf8
Luca Della Libera <luca.dellalib@gmail.com>
2022-10-19 04:43:12 +0200

    Fix experiment scripts

--- recipes/CommonVoice/Multilingual/scripts/submit_ovhai_job.sh
+++ recipes/CommonVoice/Multilingual/scripts/submit_ovhai_job.sh
@@ -10,8 +10,8 @@ CONFIG=$3
 
 ovhai job run ovhcom/ai-training-pytorch:latest \
     --name common_voice_10_0_${SIZE}_${MODEL} \
-    --flavor ai1-1-cpu \
-    --cpu 1 \
+    --flavor ai1-1-gpu \
+    --gpu 1 \
     --volume common_voice_10_0_${SIZE}@BHS/:/workspace/common_voice_10_0_${SIZE}/:RO \
     --volume common_voice_10_0_scripts@BHS/:/workspace/common_voice_10_0_scripts/:RW \
     --volume common_voice_10_0_${SIZE}_output@BHS/:/workspace/common_voice_10_0_${SIZE}_output/:RW \
