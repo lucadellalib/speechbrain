38664582849a89f12a45df4055a205e3ef3d05ed
Pooneh <mousavi.pooneh@gmail.com>
2022-11-10 11:48:40 -0500

    fix bug

--- recipes/CommonVoice/Multilingual/whisper/whisper_evaluate.py
+++ recipes/CommonVoice/Multilingual/whisper/whisper_evaluate.py
@@ -53,7 +53,7 @@ def evaluate_whisper(dataset_size,locale):
 
 
     common_voice_data = common_voice_data.map(resolve_root_dir, num_proc=4)
-    model.config.forced_decoder_ids = processor.get_decoder_prompt_ids(language=locale[0] task = "transcribe")
+    model.config.forced_decoder_ids = processor.get_decoder_prompt_ids( language=locale[0] ,task = "transcribe")
     common_voice_data = common_voice_data.rename_columns({"mp3": "audio", "wrd": "sentence"})
     common_voice_data = common_voice_data.cast_column("audio", Audio(sampling_rate=16000))
     result = common_voice_data['test'].map(map_to_pred, remove_columns=common_voice_data['test'].column_names,num_proc=1)
