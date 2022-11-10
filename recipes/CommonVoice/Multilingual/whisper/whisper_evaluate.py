812c24349cdbfe049197a66429445f095c35a3ce
Pooneh <mousavi.pooneh@gmail.com>
2022-11-10 15:16:24 -0500

    fix to work with non-defined language

--- recipes/CommonVoice/Multilingual/whisper/whisper_evaluate.py
+++ recipes/CommonVoice/Multilingual/whisper/whisper_evaluate.py
@@ -3,7 +3,7 @@ import os
 from transformers import WhisperForConditionalGeneration, WhisperProcessor,WhisperFeatureExtractor
 import soundfile as sf
 import torch
-from jiwer import wer
+from jiwer import wer,cer
 import pandas as pd
 import argparse
 
@@ -28,7 +28,7 @@ def map_to_pred(batch):
     batch["transcription"] = transcription[0]
     return batch
 
-def evaluate_whisper(dataset_size,locale):
+def evaluate_whisper(dataset_size,locales):
     # Dataset preparation (parsing CommonVoice)
 
     from common_voice_prepare import prepare_common_voice  # noqa
@@ -36,7 +36,7 @@ def evaluate_whisper(dataset_size,locale):
         dataset_size,
         dataset_dir=dataset_dir,
         manifest_dir=manifest_dir,
-        locales=locale,)
+        locales=locales,)
 
 
     common_voice_data = load_dataset(
@@ -52,12 +52,19 @@ def evaluate_whisper(dataset_size,locale):
 # 
 
 
-    common_voice_data = common_voice_data.map(resolve_root_dir, num_proc=4)
-    model.config.forced_decoder_ids = processor.get_decoder_prompt_ids( language=locale[0] ,task = "transcribe")
-    common_voice_data = common_voice_data.rename_columns({"mp3": "audio", "wrd": "sentence"})
-    common_voice_data = common_voice_data.cast_column("audio", Audio(sampling_rate=16000))
-    result = common_voice_data['test'].map(map_to_pred, remove_columns=common_voice_data['test'].column_names,num_proc=1)
-    print("WER:", wer(result["labels"], result["transcription"]))
+    for locale in locales:
+        common_voice_data = common_voice_data.map(resolve_root_dir, num_proc=4)
+        lan=None
+        if f"<|{locale}|>"  in processor.tokenizer.additional_special_tokens:
+            lan=locale
+        model.config.forced_decoder_ids = processor.get_decoder_prompt_ids(language=lan, task = "transcribe")
+        common_voice_data = common_voice_data.rename_columns({"mp3": "audio", "wrd": "sentence"})
+        common_voice_data = common_voice_data.cast_column("audio", Audio(sampling_rate=16000))
+        result = common_voice_data['test'].map(map_to_pred, remove_columns=common_voice_data['test'].column_names,num_proc=1)
+        print(f"Error rates for language: {locale}, size: {dataset_size} and whisper model: {args.whisper_model}")
+        print("WER:", wer(result["labels"], result["transcription"]))
+        print("CER:", cer(result["labels"], result["transcription"]))
+
 
 
 if __name__ == "__main__":
